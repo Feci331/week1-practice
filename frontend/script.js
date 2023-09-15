c61c8807d0b6d0cea9d30c6f98f818978abe6d60
feci <ferenchegymegi3@gmail.com>
2023-09-15 20:45:36 +0200

    base color change

---
+++ frontend/script.js
@@ -0,0 +1,12 @@
+console.log("hello world");
+console.log("i am feri");
+//setInterval(()=>console.log("Hello"),1000);
+
+let x = 10;
+let y = 5;
+//console.log(x+y);
+x+y;
+let z="csésze"
+console.log(z)
+z = "bögre"
+console.log(z);
